024530659f0c77d826c925256047bb76be2fdbcd
Maxim Elchugin <melchugin@gmail.com>
2021-12-03 05:49:28 +0300

    Add main page

--- app/big_brother/settings.py
+++ app/big_brother/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "pages.apps.PagesConfig",
     "sslserver",
 ]
 
@@ -63,7 +64,7 @@ ROOT_URLCONF = "big_brother.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,17 +123,20 @@ USE_L10N = True
 
 USE_TZ = True
 
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = path.join(BASE_DIR, "media/")
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = [
+    path.join(BASE_DIR, "static"),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-STATICFILES_DIRS = [
-    path.join(BASE_DIR, "static"),
-]
--- app/pages/urls.py
+++ app/pages/urls.py
@@ -1,7 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from .views import home_view
+from .views import home_view, image_upload_view
 
 urlpatterns = [
     path("", home_view, name="home"),
+    path("upload/", image_upload_view),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
