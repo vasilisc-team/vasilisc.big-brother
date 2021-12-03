28d540d1f526976278d9a0ba1290f378cb6e6227
Maxim Elchugin <melchugin@gmail.com>
2021-12-03 06:26:13 +0300

    Run container on `erebus`

--- app/run_report.sh
+++ app/run_report.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-curl https://api.telegram.org/bot${TG_TOKEN}/
+GIT_COMMIT=$(cat .git_commit)
+
+curl \
+    -X POST \
+    -H 'Content-Type: application/json' \
+    -d "{\"chat_id\": \"${TG_TO}\", \"text\": \"<pre>Run container from commit: ${GIT_COMMIT}</pre>\", \"disable_notification\": true, \"parse_mode\": \"html\"}" \
+    https://api.telegram.org/bot${TG_TOKEN}/sendMessage
